051192c98fbb0b0d829b74ee82abc44ac1df2396
BridgetteR <bridgetteriver@gmail.com>
2022-12-02 08:57:49 +1000

    Fixed issues, made start menu

---
+++ scripts/start-menu.js
@@ -0,0 +1,8 @@
+document.querySelector("#start").addEventListener("click", () => {
+    const menu = document.querySelector(".menu");
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+});
